8dcd18ca408211701aa3ebfee5c18ab7a3ac2959
Aniket Patil <aniket.p@reapmind.com>
2024-06-21 11:18:39 +0530

    Initial Commit

---
+++ CustomProgressBar/CustomProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomProgressBarApp.swift
+//  CustomProgressBar
+//
+//  Created by Aniket Patil on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
